cd0e9ec43264423db3f95d72f378d3947c54819c
thijsBoet <m.boet@chello.nl>
2022-07-24 14:55:29 +0200

    trouble with react-portable-text serializing rich text from all posts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	if (typeof window === 'undefined') {
+		return <></>;
+	} else {
+		return <Component {...pageProps} />;
+	}
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import type { NextPage } from 'next';
 import Head from 'next/head';
 import Header from '../components/Header';
+import Link from 'next/link';
 import { sanityClient, urlFor } from '../sanity';
 import { Post } from '../typings';
 
@@ -8,9 +8,7 @@ interface Props {
 	posts: [Post];
 }
 
-export const Home: NextPage = ({ posts }: Props) => {
-  console.log(posts);
-  
+export default function Home({ posts }: Props) {
 	return (
 		<div className='max-w-7xl mx-auto'>
 			<Head>
@@ -38,21 +36,44 @@ export const Home: NextPage = ({ posts }: Props) => {
 					alt=''
 				/>
 			</div>
-			
+			<div className='grid grid-cols-1 sm:grid-cols-2 lg-grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
+				{posts.map(post => (
+					<Link key={post._id} href={`/post/${post.slug.current}`}>
+						<div className='border rounded-lg group cursor-pointer overflow-hidden'>
+							<img
+								className='h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out'
+								src={urlFor(post.mainImage).url()!}
+								alt=''
+							/>
+							<div className='flex justify-between p-5 bg-white'>
+								<div>
+									<p className='text-lg font-bold'>{post.title}</p>
+									<p className='text-xs'>
+										{post.description} by {post.author.name}
+									</p>
+								</div>
+								<img
+									className='h-12 w-12 rounded-full'
+									src={urlFor(post.author.image).url()!}
+									alt={post.author.name}
+								/>
+							</div>
+						</div>
+					</Link>
+				))}
+			</div>
 		</div>
 	);
-};
-
-export default Home;
+}
 
 export const getServerSideProps = async () => {
 	const query = `*[_type == 'post']{
-    _id,
+		_id,
     title,
     author -> {
-    name,
-    image
-  },
+			name,
+			image
+		},
     description,
     mainImage,
     slug
